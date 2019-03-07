63ae375b18fc53993c1eb69bc956f360bf09fcc6
Jamie Zeng <jzeng3@stuy.edu>
2019-03-07 17:50:12 -0500

    found typo that was resetting pasture

--- USACO.java
+++ USACO.java
@@ -1,18 +1,12 @@
 import java.util.*;
 import java.io.*;
 public class USACO{
-  private int R;
-  private int C;
-  private int E;
-  private int N;
-  private int[][] pasture;
-  private int[] cowHerd;
 
   public static void main(String[] args){
     try{
-      Lake test = new Lake("makelake.1.in");
-      test.stomp(1,1,4);
-      bronze("makelake.1.in");
+
+
+      bronze("testCases/makelake.1.in");
     }
     catch (FileNotFoundException e){
       System.out.println("file not found");
@@ -20,11 +14,22 @@ public class USACO{
   }
 
     public static int bronze(String filename) throws FileNotFoundException{
-
+      Lake test = new Lake(filename);
+      test.stomp(1,4,4);
 	return 0;
 }
+    public static int silver(String filename){
+	return 0;
+    }
+}
+class Lake{
 
-    public class Lake{
+  private int R;
+  private int C;
+  private int E;
+  private int N;
+  private int[][] pasture;
+  private int[] cowHerd;
 
     public Lake(String filename) throws FileNotFoundException{
       File file = new File(filename);
@@ -42,19 +47,31 @@ public class USACO{
           C = Integer.parseInt(numbers[1]);
           E = Integer.parseInt(numbers[2]);
           N = Integer.parseInt(numbers[3]);
+          pasture = new int[R][C];
           System.out.println(R + " " + C+" "+E+ " "+N);
         }else if (line <= R){
-          pasture = new int[R][C];
+
           int r = line - 1;
           for (int i = 0; i < pasture[0].length; i++){
+            System.out.println("r: "+r);
             pasture[r][i] = Integer.parseInt(numbers[i]);
-            System.out.print(pasture[r][i]+" ");
+
           }
+          System.out.println(Arrays.toString(pasture[r]));
+
         }else if (line > R){
 
         }
         line++;
 
+      }
+      for (int r = 0; r < R; r++){
+        for (int c = 0; c < C; c++){
+          System.out.print(pasture[r][c]+" ");
+          if (c == C -1){
+            System.out.println("");
+          }
+        }
       }
 
     }
@@ -63,22 +80,35 @@ public class USACO{
       cowHerd = new int[9];
       int index = 0;
       if (1 <= R_s && R_s <= R-2 && 1 <= C_s && C_s <= C-2){
+
         // add the 9 elevations to an array
-        for (int i = R_s; i < R_s+2; i++){
-          cowHerd[index] = pasture[i][C_s];
-          cowHerd[index+1] = pasture[i][C_s +1];
-          cowHerd[index+2] = pasture[i][C_s +2];
+        for (int i = R_s-1; i < R_s+2; i++){
+            System.out.println("index: "+index);
+          System.out.println("current row: "+i);
+          System.out.println("current place: "+i+", "+C_s+": "+pasture[i][C_s-1]);
+          cowHerd[index] = pasture[i][C_s-1];
+          System.out.println(cowHerd[index]);
+          cowHerd[index+1] = pasture[i][C_s];
+          cowHerd[index+2] = pasture[i][C_s+1];
           index += 3;
         /*  cowHerd.add(new Cow(i, C_s, pasture[i][C_s]));
           cowHerd.add(new Cow(i, C_s+1, pasture[i][C_s+1]));
           cowHerd.add(new Cow(i, C_s+2, pasture[i][C_s+2]));*/
 
         }
+        System.out.println(Arrays.toString(cowHerd));
         Arrays.sort(cowHerd);
         int highestElev = cowHerd[8];
+        System.out.println(highestElev);
         int diff = 0;
         // update the pasture levels
-        for (int i = R_s; i < R_s+2; i++){
+        for (int i = R_s -1; i < R_s+1; i++){
+          diff = highestElev - pasture[i][C_s-1];
+          if (diff <= D_s){
+            int start = D_s - diff;
+            System.out.println("subtract by "+ start);
+            pasture[i][C_s-1] -= (D_s - diff);
+          }
           diff = highestElev - pasture[i][C_s];
           if (diff <= D_s){
             pasture[i][C_s] -= (D_s - diff);
@@ -87,10 +117,6 @@ public class USACO{
           if (diff <= D_s){
             pasture[i][C_s+1] -= (D_s - diff);
           }
-          diff = highestElev - pasture[i][C_s+2];
-          if (diff <= D_s){
-            pasture[i][C_s+2] -= (D_s - diff);
-          }
 
         /*  cowHerd.add(new Cow(i, C_s, pasture[i][C_s]));
           cowHerd.add(new Cow(i, C_s+1, pasture[i][C_s+1]));
@@ -98,15 +124,10 @@ public class USACO{
 
         }
       }
+      for (int r = 0; r < 4; r++){
+      System.out.println(Arrays.toString(pasture[r]));
+    }
+
 
     }
   }
-
-
-
-    public static int silver(String filename){
-	return 0;
-    }
-
-
-}

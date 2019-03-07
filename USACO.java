c12f421d47404b0705a508b542993bb46d859088
Jamie Zeng <jzeng3@stuy.edu>
2019-03-07 18:12:57 -0500

    bronze calculates volume successfully

--- USACO.java
+++ USACO.java
@@ -4,23 +4,23 @@ public class USACO{
 
   public static void main(String[] args){
     try{
-
-
-      bronze("testCases/makelake.1.in");
+      System.out.println(bronze("testCases/makelake.2.in"));
+    //  bronze("testCases/makelake.2.in");
+    //  bronze("testCases/makelake.3.in");
     }
     catch (FileNotFoundException e){
       System.out.println("file not found");
     }
   }
 
-    public static int bronze(String filename) throws FileNotFoundException{
-      Lake test = new Lake(filename);
-      test.stomp(1,4,4);
-	return 0;
-}
-    public static int silver(String filename){
-	return 0;
-    }
+  public static int bronze(String filename) throws FileNotFoundException{
+    Lake test = new Lake(filename);
+    System.out.println(test);
+    return test.volume();
+  }
+  public static int silver(String filename){
+    return 0;
+  }
 }
 class Lake{
 
@@ -31,103 +31,111 @@ class Lake{
   private int[][] pasture;
   private int[] cowHerd;
 
-    public Lake(String filename) throws FileNotFoundException{
-      File file = new File(filename);
-      Scanner input = new Scanner(file);
-      String readLine = "";
-      int line = 0;
-      while (input.hasNextLine()){
-          readLine = input.nextLine();
-          String[] numbers = readLine.split(" ");
-          if (line == 0){
+  public Lake(String filename) throws FileNotFoundException{
+    File file = new File(filename);
+    Scanner input = new Scanner(file);
+    String readLine = "";
+    int line = 0;
+    while (input.hasNextLine()){
+      readLine = input.nextLine();
+      String[] numbers = readLine.split(" ");
+      /* read the first line of information for
+      number of rows, columns, final elevation, and
+      number of instructions*/
+      if (line == 0){
+        R = Integer.parseInt(numbers[0]);
+        C = Integer.parseInt(numbers[1]);
+        E = Integer.parseInt(numbers[2]);
+        N = Integer.parseInt(numbers[3]);
+        pasture = new int[R][C];
+        System.out.println(R + " " + C+" "+E+ " "+N);
+      }else if (line <= R){
 
-          //System.out.println(readLine.toCharArray());
-        //  String[] numbers = readLine.split(" ");
-          R = Integer.parseInt(numbers[0]);
-          C = Integer.parseInt(numbers[1]);
-          E = Integer.parseInt(numbers[2]);
-          N = Integer.parseInt(numbers[3]);
-          pasture = new int[R][C];
-          System.out.println(R + " " + C+" "+E+ " "+N);
-        }else if (line <= R){
+        int r = line - 1;
+        for (int i = 0; i < pasture[0].length; i++){
 
-          int r = line - 1;
-          for (int i = 0; i < pasture[0].length; i++){
-            System.out.println("r: "+r);
-            pasture[r][i] = Integer.parseInt(numbers[i]);
-
-          }
-          System.out.println(Arrays.toString(pasture[r]));
-
-        }else if (line > R){
+          pasture[r][i] = Integer.parseInt(numbers[i]);
 
         }
-        line++;
 
+
+      }else if (line > R){
+        int R_s = Integer.parseInt(numbers[0]);
+        int C_s = Integer.parseInt(numbers[1]);
+        int D_s = Integer.parseInt(numbers[2]);
+        stomp(R_s, C_s, D_s);
       }
-      for (int r = 0; r < R; r++){
-        for (int c = 0; c < C; c++){
-          System.out.print(pasture[r][c]+" ");
-          if (c == C -1){
-            System.out.println("");
-          }
-        }
-      }
+      line++;
 
     }
 
-    public void stomp(int R_s, int C_s, int D_s){
-      cowHerd = new int[9];
-      int index = 0;
-      if (1 <= R_s && R_s <= R-2 && 1 <= C_s && C_s <= C-2){
+  }
 
-        // add the 9 elevations to an array
-        for (int i = R_s-1; i < R_s+2; i++){
-            System.out.println("index: "+index);
-          System.out.println("current row: "+i);
-          System.out.println("current place: "+i+", "+C_s+": "+pasture[i][C_s-1]);
-          cowHerd[index] = pasture[i][C_s-1];
-          System.out.println(cowHerd[index]);
-          cowHerd[index+1] = pasture[i][C_s];
-          cowHerd[index+2] = pasture[i][C_s+1];
-          index += 3;
-        /*  cowHerd.add(new Cow(i, C_s, pasture[i][C_s]));
-          cowHerd.add(new Cow(i, C_s+1, pasture[i][C_s+1]));
-          cowHerd.add(new Cow(i, C_s+2, pasture[i][C_s+2]));*/
-
-        }
-        System.out.println(Arrays.toString(cowHerd));
-        Arrays.sort(cowHerd);
-        int highestElev = cowHerd[8];
-        System.out.println(highestElev);
-        int diff = 0;
-        // update the pasture levels
-        for (int i = R_s -1; i < R_s+1; i++){
-          diff = highestElev - pasture[i][C_s-1];
-          if (diff <= D_s){
-            int start = D_s - diff;
-            System.out.println("subtract by "+ start);
-            pasture[i][C_s-1] -= (D_s - diff);
-          }
-          diff = highestElev - pasture[i][C_s];
-          if (diff <= D_s){
-            pasture[i][C_s] -= (D_s - diff);
-          }
-          diff = highestElev - pasture[i][C_s+1];
-          if (diff <= D_s){
-            pasture[i][C_s+1] -= (D_s - diff);
-          }
-
-        /*  cowHerd.add(new Cow(i, C_s, pasture[i][C_s]));
-          cowHerd.add(new Cow(i, C_s+1, pasture[i][C_s+1]));
-          cowHerd.add(new Cow(i, C_s+2, pasture[i][C_s+2]));*/
-
+  public String toString(){
+    String pastureStr = "";
+    for (int r = 0; r < R; r++){
+      for (int c = 0; c < C; c++){
+        pastureStr += pasture[r][c]+" ";
+        if (c == C -1){
+          pastureStr += "\n";
         }
       }
-      for (int r = 0; r < 4; r++){
-      System.out.println(Arrays.toString(pasture[r]));
     }
+    return pastureStr;
+  }
 
+  public void stomp(int R_s, int C_s, int D_s){
+    cowHerd = new int[9];
+    int index = 0;
+    if (1 <= R_s && R_s <= R-2 && 1 <= C_s && C_s <= C-2){
+      // add the 9 elevations to an array
+      for (int i = R_s-1; i < R_s+2; i++){
+        cowHerd[index] = pasture[i][C_s-1];
+        cowHerd[index+1] = pasture[i][C_s];
+        cowHerd[index+2] = pasture[i][C_s+1];
+        index += 3;
+      }
 
+      Arrays.sort(cowHerd);
+      int highestElev = cowHerd[8];
+      int diff = 0;
+
+      // update the pasture levels
+      for (int i = R_s -1; i < R_s+2; i++){
+        diff = highestElev - pasture[i][C_s-1];
+        if (diff <= D_s){
+          int start = D_s - diff;
+
+          pasture[i][C_s-1] -= (D_s - diff);
+        }
+        diff = highestElev - pasture[i][C_s];
+        if (diff <= D_s){
+          pasture[i][C_s] -= (D_s - diff);
+        }
+        diff = highestElev - pasture[i][C_s+1];
+        if (diff <= D_s){
+          pasture[i][C_s+1] -= (D_s - diff);
+        }
+
+      }
     }
   }
+
+  public int volume(){
+    int totalDepth = 0;
+    for (int r = 0; r < R; r++){
+      for (int c = 0; c < C; c++){
+        if ( (E - pasture[r][c]) > 0){
+          totalDepth += (E - pasture[r][c]);
+        }
+      }
+    }
+    System.out.println("total Depth: "+ totalDepth);
+    int length = R * 6 * 12;
+    System.out.println("L: "+ length);
+    int width = C * 6* 12;
+    System.out.println("W: "+ width);
+    int volume = totalDepth * 72 * 72;
+    return volume;
+  }
+}

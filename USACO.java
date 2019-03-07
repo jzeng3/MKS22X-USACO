2eb57055141007f506e28c98eff30cc7fe25b5cd
Jamie Zeng <jzeng3@stuy.edu>
2019-03-07 10:04:03 -0500

    Started stomp code and Cow class

--- USACO.java
+++ USACO.java
@@ -1,6 +1,12 @@
 import java.util.*;
 import java.io.*;
 public class USACO{
+  private int R;
+  private int C;
+  private int E;
+  private int N;
+  private int[][] pasture;
+
   public static void main(String[] args){
     try{
       bronze("makelake.1.in");
@@ -9,16 +15,12 @@ public class USACO{
       System.out.println("file not found");
     }
   }
+
     public static int bronze(String filename) throws FileNotFoundException{
       File file = new File(filename);
       Scanner input = new Scanner(file);
-      int line = 0;
-      int R = 0;
-      int C = 0;
-      int E = 0;
-      int N = 0;
-      int[][] pasture;
       String readLine = "";
+      line = 0;
       while (input.hasNextLine()){
           readLine = input.nextLine();
           String[] numbers = readLine.split(" ");
@@ -31,18 +33,40 @@ public class USACO{
           E = Integer.parseInt(numbers[2]);
           N = Integer.parseInt(numbers[3]);
           System.out.println(R + " " + C+" "+E+ " "+N);
-        }else if (line < R){
+        }else if (line <= R){
           pasture = new int[R][C];
           int r = line - 1;
           for (int i = 0; i < pasture[0].length; i++){
             pasture[r][i] = Integer.parseInt(numbers[i]);
             System.out.print(pasture[r][i]+" ");
           }
+        }else if (line > R){
+
         }
         line++;
 
       }
 	return 0;
+    }
+
+    public void stomp(int R_s, int C_s, int D_s){
+
+      if (1 <= R_s && R_s <= R-2 && 1 <= C_s && C_s <= C-2){
+        for (int i = R_s; i < R_s+2; i++){
+
+        }
+      }
+    }
+    private class Cow implements Comparable<Cow>{
+      public int row, col, elev;
+      public Cow(int r, int c, int e){
+        row = r;
+        col = c;
+        elev = e;
+      }
+      public int compareTo(Cow other){
+        return elev - other.elev;
+      }
     }
     public static int silver(String filename){
 	return 0;
